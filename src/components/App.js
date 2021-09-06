c5f3b7a626512dc1fe1cda4cc80bdcfbe56a0a76
piotrnajda3000 <najdapiotr79@gmail.com>
2021-09-06 13:24:15 +0200

    Style init

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 
-const App = () => {
-  return <div>App</div>;
+const App = ({ data }) => {
+  return (
+    <>
+      <div id="gameInfo">
+        <div id="scoreCurrent">Current {data.score.current}</div>
+        <div id="gameWon">{data.isWon ? "You won!" : null}</div>
+        <div id="scoreHigh"> Best {data.score.high} </div>
+      </div>
+      <div id="cardsContainer">
+        {data.cards.map((card) => {
+          return (
+            <div className="card" key={card.id}>
+              {card.text}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default App;
